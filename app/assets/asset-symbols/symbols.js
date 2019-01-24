f2ad7d348c7d1ffb51959f2ca75b4321984a9cc1
quattrocento <46367535+quattrocento@users.noreply.github.com>
2019-01-25 00:21:00 +0800

    Update symbols.js

--- app/assets/asset-symbols/symbols.js
+++ app/assets/asset-symbols/symbols.js
@@ -1,5 +1,5 @@
 // Core asset
-require("file-loader?name=asset-symbols/[name].png!./bts.png");
+require("file-loader?name=asset-symbols/[name].png!./eon.png");
 
 // BitAssets
 require("file-loader?name=asset-symbols/[name].png!./usd.png");
